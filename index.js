846d99ccc8a580f1f59a786ba60993636bf72f72
Rasun Manohara <rasunmanohara@gmail.com>
2025-03-07 22:41:47 +0530

    Create the gitignore file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, This is Your First App Hii');
+    res.send('Hello, Welcome to the App!');
 });
 
 app.listen(3000, () => {
